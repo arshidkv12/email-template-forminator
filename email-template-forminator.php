da9b3cf7a331c04d3c2d95af9ac9a7b9a9cefb16
Arshid <arshidkv12@gmail.com>
2023-10-07 10:06:42 +0530

    Update email-template-forminator.php

--- email-template-forminator.php
+++ email-template-forminator.php
@@ -1,12 +1,17 @@
 <?php 
 /**
  * Plugin Name: Email Template for Forminator
+ * Description: Custom email template
+ * Version: 1.0.0
+ * Author: Arshid
  */
 
 new EmailTemplate4Forminator;
 class EmailTemplate4Forminator{
 
     public $msg;
+    public $tags;
+    public $values;
 
     public function __construct()
     {
@@ -17,13 +22,20 @@ class EmailTemplate4Forminator{
 
 
     public function msg( $message, $custom_form, $data, $entry, $mail ){
-        $this->msg = $message;
+        $this->msg    = $message;
+        $keys         = array_keys( $data );
+        $this->values = array_values( $data );
+        $this->tags   = array_map(function($item){
+                            return '{'.$item.'}';
+                        }, $keys);
+                
         return $message;
     }
 
     public function email4frm_clean( $body ){
         $template = file_get_contents( plugin_dir_path(__FILE__) .'/template.php' );
         $body = str_replace( '{msg}', $this->msg, $template);
+        $body = str_replace( $this->tags, $this->values, $body);
         return $body;
     }
 }
